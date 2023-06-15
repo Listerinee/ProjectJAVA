d11a4c119e2bbf4ff8434118270d1aa718107d83
Vitor Luis Ferreira <83659795+Listerinee@users.noreply.github.com>
2023-06-15 19:34:23 -0300

    Automovel class
    Estudo de criação de classes e estancia

---
+++ Automovel/src/Projeto/src/Automovel.java
@@ -0,0 +1,12 @@
+class Automovel {
+    String cor;
+    String modelo;
+    Date ano;
+
+    // Atributos
+    
+    void liga (){
+        System.out.println("O automovel está ligado!");
+    }
+
+}
---
+++ Automovel/src/Projeto/src/Projeto.java
@@ -0,0 +1,11 @@
+class Projeto{
+    public static void main (String[] args){
+        Automovel fusca;
+        fusca = new Automovel ();        
+        
+        fusca.cor = "Branco";
+        fusca.modelo = "VolksWagen";
+
+        System.out.println("Cor do Automovel: " + fusca.cor);
+}
+}
